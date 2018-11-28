efefe59726c56f24647274085c3af414c2983091
Laura Shamus <lkshamus@gmail.com>
2018-11-28 15:38:46 -0700

    Add function to toggle on and off class

--- public/scripts.js
+++ public/scripts.js
@@ -31,56 +31,17 @@ document.addEventListener("DOMContentLoaded", setRandomColorText);
 function lockColor(e) {
   for (let i = 0; i < 6; i ++) {
     if (e.target.classList.contains(`color-palette${i}`)) {
+      console.log('do u work')
       let color = $(this).css('background-color')
-      console.log(color)
-      $(`.color-palette${i}`).addClass(`new-class${i}`)
-      // $(`new-class${i}`).css('color', color)
-      // $(`.color-palette${i}`).toggleClass(`new-class${i}`)
-      $(`.new-class${i}`).toggleClass(`color-palette${i}`)
+      $(`new-class${i}`).css('color', color)
+      $(`.color-palette${i}`).toggleClass(`new-class${i}`, `.lock`)
     }
   }
 }
-
-function unlockColor(e) {
-    if (e.target.classList.contains(`new-class1`)) {
-      for (let i = 0; i < 6; i ++) {
-        console.log('is this WORKING')
-      let color = $(this).css('background-color')
-      console.log(color)
-      $(`color-palette${i}`).removeClass(`new-class${i}`)
-      // if (e.target.classList.contains(`new-class${i}`)){
-      //   $(`.color-palette${i}`).removeClass(`new-class${i}`)
-      }
-    }
-  }
-
 
 document.querySelector('.color-palette1').addEventListener('click', lockColor)
 document.querySelector('.color-palette2').addEventListener('click', lockColor)
 document.querySelector('.color-palette3').addEventListener('click', lockColor)
 document.querySelector('.color-palette4').addEventListener('click', lockColor)
 document.querySelector('.color-palette5').addEventListener('click', lockColor)
-// document.querySelector('.color-palette1').addEventListener('click', unlockColor)
-// document.querySelector('.color-palette2').addEventListener('click', unlockColor)
-// document.querySelector('.color-palette3').addEventListener('click', unlockColor)
-// document.querySelector('.color-palette4').addEventListener('click', unlockColor)
-// document.querySelector('.color-palette5').addEventListener('click', unlockColor)
 
-
-function toggleLock() {
-
-}
-
-// document.querySelector('.color-palette').addEventListener('click', function() {
-//     let color = $(this).css('background-color')
-//     console.log(color)
-// })
-
-// function lockColor() {
-//   for (let i = 0; i < 6; i++) {
-//     let color = $(`.color-palette${i}`).css('background-color')
-//     console.log(color)
-//   }
-// }
-
-// document.querySelector('.color-palette').addEventListener('click', lockColor)
