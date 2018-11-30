d4694c0811e1f2d832f765de3bd11a318ce9097a
Laura Shamus <lkshamus@gmail.com>
2018-11-29 17:52:33 -0700

    Add working backend woohoo

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ app.get('/', (request, response) => {
 
 app.locals.title = 'Palette Picker'
 
-app.locals.palettes = [
-  {id: 1, name: 'project1', hex1: '12345', hex2: '23456', hex3: '345678', hex4: '45678', hex5: '56789', projectId: 1}
-]
-
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
     .then(projects => {
@@ -41,8 +37,42 @@ app.get('/api/v1/projects/palettes', (request, response) => {
       })
 })
 
+app.post('/api/v1/projects', (request, response) => {
 
+  const project = request.body
 
+  for(let requiredParam of ['title']) {
+    if(!paper[requiredParam]) {
+      response.status(422).json({ error: 'Missing required param' })
+    }
+  }
+
+  database('projects').insert(project, 'id')
+    .then(projectIds => {
+      response.status(201).json({ id: projectIds[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error: error.message })
+    })
+})
+
+app.get('/api/v1/projects/:id', (request, response) => {
+  const { id } = request.params
+
+  database('projects').where('id', id).select()
+    .then(project => response.status(200).json(project))
+    .catch(error => console.log(`Error fetching project: ${error.message}`))
+})
+
+app.get('/api/v1/projects/palettes', (request, response) => {
+  database('palettes').select()
+    .then(palettes => {
+      response.status(200).json(palettes)
+    })
+    .catch(error => {
+      response.status(500).json({ error: error.message })
+    })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`);
